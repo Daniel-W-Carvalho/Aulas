f629cf47c2e0eabfd985389a7e94af47c18d38aa
Daniel Carvalho <danielcarvalho7122@gmail.com>
2026-02-25 15:11:15 -0300

    Exercicio TypeScript em Funções

---
+++ 05-Aulas TypeScript/01-instalando-ts-e-compilando/03-Tipagens-em-funcoes/index.ts
@@ -0,0 +1,36 @@
+renderProducts();
+
+type Product = {
+    id: number
+    title: string
+    price: number
+    description: string
+    category: string
+    image: string
+    rating: {
+        rate: number
+        count: number
+    }
+}
+
+async function getProducts(): Promise<Product[]> {
+    const response = await fetch('https://fakestoreapi.com/products');
+    const data = await response.json() as Product[]; 
+    return data;
+}
+
+async function renderProducts() {
+    try {
+        const products = await getProducts()
+
+        products.forEach(product => {
+            console.log(`Título: ${product.title}`)
+            console.log(`Preço: $${product.price}`)
+        })
+    } catch (error) {
+        console.error('Erro ao carregar produtos', error)
+    }
+}
+
+renderProducts();
+
